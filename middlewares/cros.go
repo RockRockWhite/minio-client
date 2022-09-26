a30aab060eb82e254f570952f2a4c3e7e15b00d7
RockRockWhite <1834309151@qq.com>
2022-09-26 14:37:07 +0800

    fix cros

--- middlewares/cros.go
+++ middlewares/cros.go
@@ -8,8 +8,8 @@ import (
 func Cors(context *gin.Context) {
 	context.Header("Access-Control-Allow-Origin", "*")
 	context.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
-	context.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Token")
-	context.Header("Access-Control-Expose-Headers", "Access-Control-Allow-Headers, Token")
+	context.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Authorization")
+	context.Header("Access-Control-Expose-Headers", "Access-Control-Allow-Headers, Authorization")
 	context.Header("Access-Control-Allow-Credentials", "true")
 	if context.Request.Method == "OPTIONS" {
 		context.AbortWithStatus(http.StatusNoContent)
